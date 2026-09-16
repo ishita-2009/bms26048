660b1e13c510abd66deb7cac5e7ad81905816d97
ishita-2009 <ishita.c2009@gmail.com>
2026-09-16 10:38:35 +0530

    Update data.js

--- data.js
+++ data.js
@@ -21,23 +21,21 @@ const site = {
 const projects = [
   {
     number: "01",
-    title: "PROJECT 01 — ADD TITLE",
-    date: "TBD",
+    title: "PROJECT 01 — Group Project",
+    date: "15th September",
     category: "IIS",
     accent: "blue",
-    description: "Write a short description of what this project was about.",
-    contribution: "Write a short description of what you personally worked on.",
+    description: "About:",
+    contribution: "What I worked on: (files will be attached below)",
     learnings: [
-      "Add your first key learning.",
-      "Add your second key learning.",
-      "Add your third key learning."
+      "Key learning.",
     ],
     files: [],
     images: []
   },
   {
     number: "02",
-    title: "PROJECT 02 — ADD TITLE",
+    title: "PROJECT 02 —(not given yet)",
     date: "TBD",
     category: "IIS",
     accent: "violet",
@@ -53,7 +51,7 @@ const projects = [
   },
   {
     number: "03",
-    title: "PROJECT 03 — ADD TITLE",
+    title: "PROJECT 03 — (not given yet)",
     date: "TBD",
     category: "IIS",
     accent: "cyan",
@@ -69,7 +67,7 @@ const projects = [
   },
   {
     number: "04",
-    title: "PROJECT 04 — ADD TITLE",
+    title: "PROJECT 04 — (not given yet)",
     date: "TBD",
     category: "IIS",
     accent: "pink",
